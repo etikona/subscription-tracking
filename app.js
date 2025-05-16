e06ac1ee9cfb37a946dea009f73560187a5a6441
eti kona <etikonapal@gmail.com>
2025-05-16 17:49:25 +0600

    Environement variables setup in config file

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from "express";
-
+import { PORT } from "./config/env.js";
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("welcome to the subscription tracking api");
 });
 
-app.listen(3000, () => {
-  console.log("Subscription API Running on 3000");
+app.listen(PORT, () => {
+  console.log(`Subscription API Running on ${PORT}`);
 });
+
+export default app;

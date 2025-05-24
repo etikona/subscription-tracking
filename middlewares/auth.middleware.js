08d18cce7fb1418e96d14c9a86fb5df17a139663
eti kona <etikonapal@gmail.com>
2025-05-25 04:19:08 +0600

    Authorized

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,27 @@
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../config/env.js";
+import User from "../models/user.model.js";
+export const authorize = async (req, res, next) => {
+  try {
+    let token;
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startWith("Bearer")
+    ) {
+      token = req.headers.authorization.split(" ")[1];
+    }
+    if (!token) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+    const decoded = JsonWebTokenError.verify(token, JWT_SECRET);
+    const user = await User.findById(decoded.userId);
+    if (!user) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Unauthorized", error: error.message });
+  }
+};
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-
+import { getUser, getUsers } from "../controllers/user.controller.js";
+import { authorize } from "../middlewares/auth.middleware.js";
 const userRouter = Router();
 
-userRouter.get("/", (req, res) => res.send({ title: "Get all users" }));
-userRouter.get("/:id", (req, res) => res.send({ title: "Get a single user" }));
+userRouter.get("/", getUsers);
+userRouter.get("/:id", authorize, getUser);
 userRouter.post("/", (req, res) => res.send({ title: "Set all users" }));
 userRouter.put("/", (req, res) => res.send({ title: "Update all users" }));
 userRouter.delete("/", (req, res) => res.send({ title: "Delete all users" }));
